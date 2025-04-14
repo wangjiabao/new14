037366a191618bb43d4b7a4e72485d2c56914305
wangjiabao <840028401@qq.com>
2025-04-14 19:19:47 +0800

    ok

--- app/app/internal/biz/user.go
+++ app/app/internal/biz/user.go
@@ -3074,7 +3074,7 @@ func (uuc *UserUseCase) AdminDailyLocationRewardNewTwo(ctx context.Context, req 
 	)
 
 	//buyRecords, err = uuc.repo.GetBuyRecord(ctx, -9)
-	buyRecords, err = uuc.repo.GetBuyRecord(ctx, -8)
+	buyRecords, err = uuc.repo.GetBuyRecord(ctx, -9)
 	if nil != err {
 		fmt.Println("今日分红错误用户获取失败")
 		return nil, nil
